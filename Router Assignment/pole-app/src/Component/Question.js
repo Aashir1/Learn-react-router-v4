e0fa1f91309a14daee5c549c3267779a695aec88
Aashir Khan <kaashir9@gmail.com>
2017-12-16 09:18:54 +0500

    work on pole options percentage

--- Router Assignment/pole-app/src/Component/Question.js
+++ Router Assignment/pole-app/src/Component/Question.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import fire from '../firebase';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+
+const styles = {
+    block: {
+      maxWidth: 250,
+    },
+    radioButton: {
+      marginBottom: 16,
+    },
+  };
 
 class Question extends React.Component {
     constructor() {
         super();
         this.state = {
-            poleNameAndKey : []
+            poleNameAndKey: [],
+            radioGroup: [false, false, false, false]
         };
         this.database = fire.database().ref('/pole-App');
     }
@@ -16,12 +32,13 @@ class Question extends React.Component {
             let tempArray = this.state.poleNameAndKey;
             obj['name'] = snapshot.val().poleName;
             obj['key'] = snapshot.key;
+            obj['question'] = snapshot.val().question;
             obj['options'] = snapshot.val().options;
             tempArray.push(obj);
             this.setState({
-                // poleNameAndKey: [...this.state.poleNameAndKey, obj]
-                poleNameAndKey : tempArray
+                poleNameAndKey: tempArray
             });
+            console.log(snapshot.val());
             // let obj = {};
             // obj['name'] = snapshot.val().poleName;
             // obj['key'] = snapshot.key;
@@ -32,10 +49,85 @@ class Question extends React.Component {
             console.log('poleNameAndKey: ', this.state.poleNameAndKey);
         })
     }
+    handleRadio = (event) =>{
+        // let tempArray = this.state.radioGroup;
+        // for (let i = 0; i < tempArray.length; i++) {
+        //     if(i === indx){
+        //         tempArray[i] = true;
+        //     }
+        //     else{
+        //         tempArray[i] = false;
+        //     }
+        // }
+        // this.setState({
+        //     radioGroup: tempArray
+        // })
+        console.log(event.target.value);
+    }
 
     render() {
+        console.log(this.state.poleNameAndKey);
         return (
-            <div>{this.props.match.params.poleName}</div>
+            <div>
+                {
+                    this.state.poleNameAndKey.map((obj) => {
+                        return (
+                            (obj.name === this.props.match.params.poleName) ?
+                                <div>
+                                    <h1>{obj.name}</h1>
+                                    <h2>
+                                        Question: 
+                                        <br />
+                                        {obj.question}
+                                    </h2>
+                                    <div>
+                                    <MuiThemeProvider>
+                                        <RadioButtonGroup name="shipSpeed" defaultSelected="not_light" onChange={(event) => { this.handleRadio(event) }}>
+                                    {
+                                        
+
+                                        obj.options.map((eachOption, i) => {
+                                            return (
+                                                <RadioButton
+                                                  value={`${eachOption}`}
+                                                  label={`${eachOption}`}
+                                                  checked={this.state.radioGroup[i]}
+                                                //   onChange={(event) => { this.handleRadio(event, i) }}
+                                                  style={styles.radioButton}
+                                                />
+                                                // <label>
+                                                //     <input type="radio"
+                                                //         name="radioGroup"
+                                                //         value={eachOption}
+                                                //         checked={this.state.radioGroup[i]}
+                                                //         onChange={(event) => { this.handleRadio(event, i) }}
+                                                //     />
+                                                //     {eachOption}
+                                                // </label>
+                                            )
+                                        })
+                                        
+                                        
+                                    }
+                                    </RadioButtonGroup>
+                                    </MuiThemeProvider>
+
+                                    </div>
+                                </div> :
+                                <div></div>
+                        )
+
+
+                    })
+
+
+
+                }
+
+                <div>{this.props.match.params.poleName}</div>
+            </div>
+
+
         )
     }
 }
